626030c1eac5a73706aae4206c5fbea087b1d289
Huajie Yin <63864156+y234xt@users.noreply.github.com>
2024-04-22 18:39:30 +0000

    added HTML and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask  # Import Flask Class
+from flask import Flask, render_template 
 
-app = Flask(__name__)  # Create an Instance
+app = Flask(__name__)  
 
 
 @app.route("/")
 def hello_world():
-  return "Hello, World!"
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
